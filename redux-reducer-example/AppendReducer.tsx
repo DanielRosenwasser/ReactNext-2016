9e1cde9ba7ca7ad7272d0050d555181d01fc5fe8
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2016-09-20 15:25:56 -0400

    Added updates for redux-style example.

--- redux-reducer-example/AppendReducer.tsx
+++ redux-reducer-example/AppendReducer.tsx
@@ -1,8 +1,32 @@
+interface AppendAction {
+    type: "append";
+    newContents: string;
+}
 
+interface EraseAction {
+    type: "erase";
+    numChars: number;
+}
 
-function modifyString(originalString, action) {
+type Action = AppendAction | EraseAction;
+
+/**
+ * Try hovering over 'action' in each branch using an editor with TypeScript support.
+ */
+function modifyString(originalString: string, action: Action) {
     switch (action.type) {
         case "append":
             return originalString + action.newContents;
+        case "erase":
+            return originalString.slice(0, -action.numChars);
     }
 }
+
+modifyString("hello ", {
+    type: "append",
+    newContents: "world"
+});
+
+
+
+
